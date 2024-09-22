7d30db531366a324bcff0a3ed0e108163ba651f1
YOUSEF <nanisyousof@gmail.com>
2024-09-22 12:02:47 +0200

    add the new encoder architecture

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Ribonanza-RNA-Folding
 
-Last year, Stanford hosted a competition focused on predicting RNA base reactivity using Map and 2A3 reactivity data. 
-
-To address this challenge, This repository employs an encoder-decoder architecture.  
-
-The conformer-based encoder captures global features of the RNA sequence. Subsequently, the decoder leverages positional encoding to process the data and a masking mechanism to prevent interactions between non-interacting bases, thereby refining the reactivity predictions. 
+Last year, Stanford hosted a competition aimed at predicting RNA base reactivity using Map and 2A3 reactivity data. 
+To address this task, I used a conformer model architecture, an architecture which was invented for speech recognition, but it seems to work well with sequence data.
+ incorporating two embedding layers: one for encoding the RNA bases and another for positional information. 
+The model concludes with a multi-layer perceptron (MLP) layer to generate predictions. And since the reactivity values range from 0 to greater than 1, I employed the ReLU activation function to ensure all outputs remain non-negative. 
+For the loss function, I selected L1 loss, which evaluates the absolute difference between predicted and actual values. 
+Given the presence of numerous NaN values in the dataset, I replaced these with zeros and applied a masking technique to ensure they did not influence the training process.
--- encoder.py
+++ encoder.py
@@ -8,19 +8,42 @@ from torch.utils.data import Dataset, DataLoader
 from transformers import Trainer, TrainingArguments
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-class encoder_model(nn.Module) :
-    def __init__(self, kernel_size: int, num_channels: int, num_layers: int, feed_forward: int, num_heads: int) :
+class RNA_model(nn.Module) :
+    def __init__(self, kernel_size: int, num_channels: int, num_layers: int, feed_forward = 1024, num_heads = 16) :
         super().__init__()
 
-        self.embedding = nn.Sequential(nn.Embedding(457, num_channels//4), nn.ReLU(), nn.Linear(num_channels//4, num_channels//2), 
-                                        nn.ReLU(), nn.Linear(num_channels//2, num_channels))
-
+        self.postional_embedding = nn.Sequential(nn.Embedding(457, num_channels//4), nn.Sigmoid(), nn.Linear(num_channels//4, num_channels//2), nn.ReLU())        
+        self.base_embedding = nn.Sequential(nn.Embedding(5, num_channels//4), nn.Sigmoid(), nn.Linear(num_channels//4, num_channels//2), nn.ReLU())
+        
+        self.feed_forward = nn.Sequential(nn.Linear(num_channels, num_channels*2), nn.Sigmoid(), nn.Linear(num_channels*2, num_channels), nn.ReLU())
+        
         self.encoder =  Conformer(num_channels, num_heads, feed_forward, num_layers, kernel_size)     
+        self.result = nn.Sequential(nn.Sigmoid(), nn.Linear(num_channels, num_channels//2), nn.Linear(num_channels//2, num_channels//4), 
+                                    nn.ReLU(), nn.Linear(num_channels//4, num_channels//8), nn.ReLU(), nn.Linear(num_channels//8, 2), nn.ReLU())
+        self.loss = nn.L1Loss()
+        
+    def forward(self, input_ids, length, mask, labels=None) :
 
-    def forward(self, input_ids, length, mask) :
+        mask = torch.unsqueeze(mask, dim=-1)
+        max_len = torch.max(length)
+        mask = mask[:, :max_len]
+        input_ids = input_ids[:, :max_len]
+        
+        positional_embedding = self.postional_embedding(input_ids)*mask
+        base_embedding = self.base_embedding(input_ids)*mask
+        embedding = torch.concat((positional_embedding, base_embedding), -1)
+        feed_forward = self.feed_forward(embedding) + embedding
+        
+        encoded, _ = self.encoder(feed_forward, length)
+        output = self.result(encoded)*mask
+        
+        if labels is not None :
 
-        embedding = self.embedding(input_ids)*mask
-        encoded, _ = self.encoder(embedding, length)
-
-        return encoded
+            y = labels[:, :max_len]
+            cover = y != 0
+            output *= cover
+            loss = torch.unsqueeze( self.loss(output, y), dim=0)
+            return loss
+        
+        return output
     
